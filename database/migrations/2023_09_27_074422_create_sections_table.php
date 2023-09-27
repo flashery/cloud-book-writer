871ae64a416e935e58f2be36f95cd11ff46595fc
Yves Gonzaga <yves2regun@gmail.com>
2023-09-27 15:54:44 +0800

    Install laravel breeze with react and make model and controller

---
+++ database/migrations/2023_09_27_074422_create_sections_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('book_id')->constrained(); // foreign key on book
+            $table->unsingedBigInteger('parent_id');
+            $table->string('title');
+            $table->string('content');
+            $table->integer('nesting_level')->default(0);
+            $table->integer('order')->default(0);
+            $table->foreignId('updated_by')->constrained(); // foreign key on user
+            $table->timestamps();
+
+            $table->foreign('parent_id')->references('id')->on('sections'); // foreign key on parent
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sections');
+    }
+};
